98f8f8814c518ebd49dc89c1eb52405f49f033f2
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-07-29 11:44:30 -0300

    Create objetopessoa.js

---
+++ objetopessoa.js
@@ -0,0 +1,9 @@
+const objetoPessoaX = {
+    id: 93,
+    nome: "Arisberto", idade: 26,
+    cps: "999.888.777-66"
+
+}
+
+console.log(`nome da pessoa: ${objetoPessoaX.nome}`)
+console.log(objetoPessoaX)
