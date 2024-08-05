80d25311b537ab8211205a89023707fa396b26d4
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-08-05 12:32:20 -0300

    Create objetoemobjeto.js

---
+++ objetoemobjeto.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: "Rafaela",   
+    idade: 40,
+    email: "JP@tech.com",
+    telefone: ["41 98888-2222", "41 97777-3333"]
+}
+
+cliente.endereco = {
+    rua: 'Sete de setembro',
+    numero: 954,
+    apartamento: true,
+    complemento: "ap 25"
+}
+
+
+
+console.log(cliente)
+
+console.log(cliente.endereco)
