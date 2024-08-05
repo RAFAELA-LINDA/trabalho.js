ae28274ad6658d382af315cd9f5e98716135f5a3
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-08-05 12:15:29 -0300

    Create objeto.js

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Rafaela",   
+    idade: 40,
+    email: "JP@tech.com",
+    telefone: ["41 98888-2222", "41 97777-3333"]
+}
+
+console.log(cliente)
+
+console.log(cliente.telefone)
+
+console.log(cliente)
