335042db81ba5902ab3bd43adae90dac1646ba57
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-08-19 09:02:45 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,6 @@
+const frutas = ['laranja','morango','uva','maça','banana'];
+const frutasMaisculas = [];
+for (let i = 0; i<frutas.length;i++){
+    frutasMaisculas.push(frutas[i].toUpperCase());
+}
+console.log(frutasMaisculas);
