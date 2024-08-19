ed1e3a94bd8caf7dbc5d51bcb84c4747fe36f71f
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-08-19 11:56:58 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,6 +1,6 @@
 const frutas = ['laranja','morango','uva','maça','banana'];
-const frutasMaisculas = [];
+const frutasMaiusculas = [];
 for (let i = 0; i<frutas.length;i++){
-    frutasMaisculas.push(frutas[i].toUpperCase());
+    frutasMaiusculas.push(frutas[i].toUpperCase());
 }
-console.log(frutasMaisculas);
+console.log(frutasMaiusculas);
