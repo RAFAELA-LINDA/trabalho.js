ce17cfb4d713bdd57d60444f54cf7528eb2506a5
RAFAELA-LINDA <129951657+RAFAELA-LINDA@users.noreply.github.com>
2024-08-21 09:00:12 -0300

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,17 @@
+const turma = {
+                nome:'3º tds',
+                ano: 2024,
+                professor:{
+                             nome:'Leandro',
+                            diciplina:'prog'
+                            },
+                aluno:[{nome:'rafa',
+                        idade:16,    
+                        },
+                        {nome:'vania',
+                         idade:17,
+
+                }
+                ]
+                        
+}
